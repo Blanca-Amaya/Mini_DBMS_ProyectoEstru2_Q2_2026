98532b889f10430a22968e42dbe69766cb3b60a5
Blanca-Amaya <bcilena@gmail.com>
2026-05-24 13:41:57 -0600

    2 commit

--- src/mini_dbms_proyectoestru2_q2_2026/AvailList.java
+++ src/mini_dbms_proyectoestru2_q2_2026/AvailList.java
@@ -9,12 +9,22 @@ package mini_dbms_proyectoestru2_q2_2026;
  * @author bamay
  */
 public class AvailList { // AvailList simple solo agrega los bloques sin un orden específico (normalmente al inicio o al final).
-    private NodoAvail cabeza; // Primer bloque libre
-    private NodoAvail cola; // Ultimo bloque libre
+    private NodoAvail cabeza; // Primer bloque libre (menor posicion)
+    private NodoAvail cola; // Ultimo bloque libre (mayor posicion)
     
+    // para ver si hay espacio disponible
     public boolean estaVacio() {
         return cabeza == null;
     }
     
-    
+    // contar cuantos bloques hay libre
+    public int getTamanio() {
+        int bloquesLibres = 0;
+        NodoAvail actual = cabeza; 
+        while (actual != null) {
+            bloquesLibres++;
+            actual = actual.siguiente; // referenca al nodo posterior 
+        }
+        return bloquesLibres;
+    }
 }
